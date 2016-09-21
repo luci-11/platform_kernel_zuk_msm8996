d4d10de08801725152e4f297d7244460f0d3cf81
zhoujiabo <zhoujiabo@oneplus.cn>
2016-09-21 17:15:42 +0800

    Add some Zuk modifications.
    Change-Id: Ie711de82d86a84e28931ad973f0237537f380cfe

--- net/netfilter/xt_qtaguid.c
+++ net/netfilter/xt_qtaguid.c
@@ -44,6 +44,12 @@
 #define XT_SOCKET_SUPPORTED_HOOKS \
 	((1 << NF_INET_PRE_ROUTING) | (1 << NF_INET_LOCAL_IN))
 
+#define MAX_UID 10000
+static DEFINE_SPINLOCK(pid_stat_tree_lock);
+static struct proc_dir_entry *xt_qtaguid_stats_pid_file;
+static int qtagpid_reset_stats(void);
+static int qtagpid_set_split_uid_list(const char *input);
+static LIST_HEAD(split_uid_list);
 
 static const char *module_procdirname = "xt_qtaguid";
 static struct proc_dir_entry *xt_qtaguid_procdir;
@@ -281,12 +287,12 @@ static struct tag_node *tag_node_tree_search(struct rb_root *root, tag_t tag)
 	while (node) {
 		struct tag_node *data = rb_entry(node, struct tag_node, node);
 		int result;
-		RB_DEBUG("qtaguid: tag_node_tree_search(0x%llx): "
-			 " node=%p data=%p\n", tag, node, data);
+	     //RB_DEBUG("qtaguid: tag_node_tree_search(0x%llx): "
+	     //	 " node=%p data=%p\n", tag, node, data);
 		result = tag_compare(tag, data->tag);
-		RB_DEBUG("qtaguid: tag_node_tree_search(0x%llx): "
-			 " data.tag=0x%llx (uid=%u) res=%d\n",
-			 tag, data->tag, get_uid_from_tag(data->tag), result);
+		//RB_DEBUG("qtaguid: tag_node_tree_search(0x%llx): "
+		//	 " data.tag=0x%llx (uid=%u) res=%d\n",
+		//	 tag, data->tag, get_uid_from_tag(data->tag), result);
 		if (result < 0)
 			node = node->rb_left;
 		else if (result > 0)
@@ -970,6 +976,7 @@ static struct iface_stat *iface_alloc(struct net_device *net_dev)
 	}
 	spin_lock_init(&new_iface->tag_stat_list_lock);
 	new_iface->tag_stat_tree = RB_ROOT;
+	INIT_LIST_HEAD(&new_iface->pid_stat_list);
 	_iface_stat_set_active(new_iface, net_dev, true);
 
 	/*
@@ -1314,10 +1321,17 @@ static void iface_stat_update_from_skb(const struct sk_buff *skb,
 	spin_unlock_bh(&iface_stat_list_lock);
 }
 
+#include "xt_qtaguid_pid_stat_update.c"
+
+//process which use the same uid.
 static void tag_stat_update(struct tag_stat *tag_entry,
-			enum ifs_tx_rx direction, int proto, int bytes)
+            enum ifs_tx_rx direction, int proto, int bytes,
+            char *task_comm, pid_t task_pid)
 {
 	int active_set;
+	struct split_uid *u;
+	uid_t uid_from_tag;
+
 	active_set = get_active_counter_set(tag_entry->tn.tag);
 	MT_DEBUG("qtaguid: tag_stat_update(tag=0x%llx (uid=%u) set=%d "
 		 "dir=%d proto=%d bytes=%d)\n",
@@ -1328,6 +1342,20 @@ static void tag_stat_update(struct tag_stat *tag_entry,
 	if (tag_entry->parent_counters)
 		data_counters_update(tag_entry->parent_counters, active_set,
 				     direction, proto, bytes);
+	uid_from_tag = get_uid_from_tag(tag_entry->tn.tag);
+
+	if (uid_from_tag <= MAX_UID) {
+		//Android OS:
+		pid_stat_update(tag_entry, active_set, direction, proto, bytes, task_comm, task_pid);
+	} else {
+		//App with share uid:
+		list_for_each_entry(u, &split_uid_list, list) {
+			if (uid_from_tag == u->uid) {
+				//printk("tag_stat_update found match uid:%d\n", uid_from_tag);
+				pid_stat_update(tag_entry, active_set, direction, proto, bytes, task_comm, task_pid);
+			}
+		}
+	}
 }
 
 /*
@@ -1348,6 +1376,9 @@ static struct tag_stat *create_if_tag_stat(struct iface_stat *iface_entry,
 		goto done;
 	}
 	new_tag_stat_entry->tn.tag = tag;
+	new_tag_stat_entry->pid_stat_tree = RB_ROOT;
+	new_tag_stat_entry->iface_stat = iface_entry;
+	spin_lock_init(&new_tag_stat_entry->pid_stat_list_lock);
 	tag_stat_tree_insert(new_tag_stat_entry, &iface_entry->tag_stat_tree);
 done:
 	return new_tag_stat_entry;
@@ -1355,7 +1386,8 @@ done:
 
 static void if_tag_stat_update(const char *ifname, uid_t uid,
 			       const struct sock *sk, enum ifs_tx_rx direction,
-			       int proto, int bytes)
+			       int proto, int bytes,
+			       char *task_comm, pid_t task_pid)
 {
 	struct tag_stat *tag_stat_entry;
 	tag_t tag, acct_tag;
@@ -1408,7 +1440,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 		 * Updating the {acct_tag, uid_tag} entry handles both stats:
 		 * {0, uid_tag} will also get updated.
 		 */
-		tag_stat_update(tag_stat_entry, direction, proto, bytes);
+        tag_stat_update(tag_stat_entry, direction, proto, bytes, task_comm, task_pid);
 		goto unlock;
 	}
 
@@ -1446,7 +1478,7 @@ static void if_tag_stat_update(const char *ifname, uid_t uid,
 		 */
 		BUG_ON(!new_tag_stat);
 	}
-	tag_stat_update(new_tag_stat, direction, proto, bytes);
+	tag_stat_update(new_tag_stat, direction, proto, bytes, task_comm, task_pid);
 unlock:
 	spin_unlock_bh(&iface_entry->tag_stat_list_lock);
 	spin_unlock_bh(&iface_stat_list_lock);
@@ -1697,8 +1729,9 @@ static struct sock *qtaguid_find_sk(const struct sk_buff *skb,
 }
 
 static void account_for_uid(const struct sk_buff *skb,
-			    const struct sock *alternate_sk, uid_t uid,
-			    struct xt_action_param *par)
+                           const struct sock *alternate_sk, uid_t uid,
+                           struct xt_action_param *par,
+                           char *task_comm, pid_t task_pid)
 {
 	const struct net_device *el_dev;
 	enum ifs_tx_rx direction;
@@ -1713,7 +1746,8 @@ static void account_for_uid(const struct sk_buff *skb,
 	if_tag_stat_update(el_dev->name, uid,
 			   skb->sk ? skb->sk : alternate_sk,
 			   direction,
-			   proto, skb->len);
+			   proto, skb->len,
+			   task_comm, task_pid);
 }
 
 static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
@@ -1804,22 +1838,8 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 		 * couldn't find the owner, so for now we just count them
 		 * against the system.
 		 */
-		/*
-		 * TODO: unhack how to force just accounting.
-		 * For now we only do iface stats when the uid-owner is not
-		 * requested.
-		 */
-#ifdef SAVE_LAST_UID
-		if (!(info->match & XT_QTAGUID_UID)) {
-			protocol = ipx_proto(skb, par);
-			get_dev_and_dir(skb, par, &dir, &net_dev);
-			last_uid = get_last_uid(protocol, dir, par->hooknum, net_dev->name);
-			account_for_uid(skb, sk, last_uid, par);
-		}
-#else
-		if (!(info->match & XT_QTAGUID_UID))
-			account_for_uid(skb, sk, 0, par);
-#endif
+		if (do_tag_stat)
+			account_for_uid(skb, sk, 0, par, "LostOwner", 0);
 		MT_DEBUG("qtaguid[%d]: leaving (sk?sk->sk_socket)=%p\n",
 			par->hooknum,
 			sk ? sk->sk_socket : NULL);
@@ -1833,36 +1853,17 @@ static bool qtaguid_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	filp = sk->sk_socket->file;
 	if (filp == NULL) {
 		MT_DEBUG("qtaguid[%d]: leaving filp=NULL\n", par->hooknum);
-#ifdef SAVE_LAST_UID
-		protocol = ipx_proto(skb, par);
-		get_dev_and_dir(skb, par, &dir, &net_dev);
-		last_uid = get_last_uid(protocol, dir, par->hooknum, net_dev->name);
-		account_for_uid(skb, sk, last_uid, par);
-#else
-		account_for_uid(skb, sk, 0, par);
-#endif
+		if (do_tag_stat)
+			account_for_uid(skb, sk, 0, par, "LostFile", 0);
 		res = ((info->match ^ info->invert) &
 			(XT_QTAGUID_UID | XT_QTAGUID_GID)) == 0;
 		atomic64_inc(&qtu_events.match_no_sk_file);
 		goto put_sock_ret_res;
 	}
 	sock_uid = filp->f_cred->fsuid;
-	/*
-	 * TODO: unhack how to force just accounting.
-	 * For now we only do iface stats when the uid-owner is not requested
-	 */
-#ifdef SAVE_LAST_UID
-	if (!(info->match & XT_QTAGUID_UID)) {
-		/* 0 root user, 1000 system user */
-		protocol = ipx_proto(skb, par);
-		get_dev_and_dir(skb, par, &dir, &net_dev);
-		set_last_uid(from_kuid(&init_user_ns, sock_uid), protocol, dir, par->hooknum, (const char *)net_dev->name);
-		account_for_uid(skb, sk, from_kuid(&init_user_ns, sock_uid), par);
-	}
-#else
-	if (!(info->match & XT_QTAGUID_UID))
-		account_for_uid(skb, sk, from_kuid(&init_user_ns, sock_uid), par);
-#endif
+	if (do_tag_stat)
+		account_for_uid(skb, sk, from_kuid(&init_user_ns, sock_uid), par, sk->sk_socket->cmdline, 0);
+
 	/*
 	 * The following two tests fail the match when:
 	 *    id not in range AND no inverted condition requested
@@ -2582,6 +2583,15 @@ static ssize_t qtaguid_ctrl_parse(const char *input, size_t count)
 	case 'u':
 		res = ctrl_cmd_untag(input);
 		break;
+	case 'r':
+		res = qtagpid_reset_stats();
+		break;
+
+	case 'n':
+        printk("------ peirs  qtaguid_ctrl_parse case n begin. -----\n");
+		res = qtagpid_set_split_uid_list(input);
+        printk("------ peirs  qtaguid_ctrl_parse case n end. -----\n");
+		break;
 
 	default:
 		res = -EINVAL;
@@ -2619,6 +2629,7 @@ struct proc_print_info {
 	tag_t tag; /* tag found by reading to tag_pos */
 	off_t tag_pos;
 	int tag_item_index;
+	struct pid_stat *ps_entry;
 };
 
 static void pp_stats_header(struct seq_file *m)
@@ -3043,6 +3054,8 @@ static const struct file_operations proc_qtaguid_stats_fops = {
 	.release	= seq_release_private,
 };
 
+#include "xt_qtaguid_proc_qtaguid_stats_pid_fops.c"
+
 /*------------------------------------------*/
 static int __init qtaguid_proc_register(struct proc_dir_entry **res_procdir)
 {
@@ -3079,6 +3092,18 @@ static int __init qtaguid_proc_register(struct proc_dir_entry **res_procdir)
 	 * TODO: add support counter hacking
 	 * xt_qtaguid_stats_file->write_proc = qtaguid_stats_proc_write;
 	 */
+
+	xt_qtaguid_stats_pid_file = proc_create_data("stats_pid", proc_stats_perms,
+			*res_procdir,
+			&proc_qtaguid_stats_pid_fops,
+			NULL);
+	if (!xt_qtaguid_stats_pid_file) {
+		pr_err("qtaguid: failed to create xt_qtaguid/stats_pid "
+				"file\n");
+		ret = -ENOMEM;
+		goto no_stats_entry;
+	}
+
 	return 0;
 
 no_stats_entry:
